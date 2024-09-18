2e8d8965fe3756f913110120e408e4e0ac87b667
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-18 14:46:16 +0700

    Chỉnh sửa để upload nhiều tập tin

--- cloudinary/cloudinary.js
+++ cloudinary/cloudinary.js
@@ -2,17 +2,39 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-function uploadCloud(folderName) {
+function uploadCloud(folderName, isMultipleFile = false) {
   const storage = new CloudinaryStorage({
     cloudinary,
-    params: {
-      folder: `${folderName.trim()}`,
-      allowedFormats: ["jpeg", "png", "jpg"],
-      public_id: (req, file) =>
-        `${Date.now()}_${file.originalname.slice(
-          0,
-          file.originalname.indexOf(".")
-        )}`,
+    params: async (req, file) => {
+      let folder = `${folderName.trim()}`;
+      let public_id = `${Date.now()}_${file.originalname.slice(
+        0,
+        file.originalname.indexOf(".")
+      )}`;
+      // Check upload multiple file , if upload multiple file then create folder child with slug
+      if (isMultipleFile) {
+        const arrayKeys = Object.keys(req.body);
+        if (arrayKeys.length > 0) {
+          let slug = "";
+          const keyItem = arrayKeys.find((item) => {
+            return item.toLowerCase().indexOf("slug") !== -1;
+          });
+          if (keyItem) {
+            slug = req.body[keyItem];
+            folder += `/${slug}`;
+          } else {
+            folder += `/${public_id}`;
+          }
+        } else {
+          folder += `/${public_id}`;
+        }
+      }
+
+      return {
+        folder,
+        allowedFormats: ["jpeg", "png", "jpg"],
+        public_id,
+      };
     },
   });
   return multer({ storage });
