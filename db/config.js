e1c2cee0744c119ea5af5db99b55c65bfbb7bbc5
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-29 22:58:14 +0700

    update cofing, .env

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
 const config = {
   db: {
-    host: process.env.HOST,
-    port: process.env.PORT_DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
   listPerPage: 2,
 };
