8dc23c6f9ed0953e78268ea591b34a1f2ea5911d
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-29 22:13:16 +0700

    update config

--- db/config.js
+++ db/config.js
@@ -1,6 +1,7 @@
 const config = {
   db: {
     host: process.env.HOST,
+    port: process.env.PORT_DATABASE,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
